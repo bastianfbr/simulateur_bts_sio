b998f29269f3cb6602d22f1afda101cdb6339767
bastianfbr <badmaxyt@gmail.com>
2022-05-14 22:59:41 +0200

    ⚡️Handling app.php
    Handling app.php to perform the calculation of the average separately from this file

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include 'app.php';
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -67,40 +71,4 @@
     <h3> La moyenne du BTS SIO est de : </h3>
     <h2 id="note"> Veuillez entrer vos notes </h2>
   </body>
-</html> <?php
-  if(isset($_GET['submit']))
-  {
-    $culture = $_GET['culture'] * 2;
-    $anglais = $_GET['anglais'] * 2;
-    $maths_ecrit = $_GET['maths_ecrit'] * 2;
-    $maths_algo = $_GET['maths_algo'];
-    $cejm = $_GET['cejm'] * 3;
-    $e4 = $_GET['e4'] * 4;
-    $e5 = $_GET['e5'] * 4;
-    $e6 = $_GET['e6'] * 4;
-    $lv2 = $_GET['lv2'];
-    $math_app = $_GET['math_app'];
-    $certif = $_GET['certif'];
-    $total = $culture + $anglais + $maths_ecrit + $maths_algo + $cejm + $e4 + $e5 + $e6;
-    if ($lv2 > 10) {
-        $lv2 = $lv2 - 10;
-        $total = $total + $lv2;
-    }
-    if ($math_app > 10) {
-        $math_app = $math_app - 10;
-        $total = $total + $math_app;
-    }
-    if ($certif > 10) {
-        $certif = $certif - 10;
-        $total = $total + $certif;
-    }
-    $moyenne = $total / 22;
-echo "
-																							<script type=\"text/javascript\">
-    document.addEventListener(\"DOMContentLoaded\", function() {
-    var note = document.getElementById(\"note\");
-    note.innerHTML = \"$moyenne\";
-    });
-</script>"; 
-  }
-?>
+</html> 
